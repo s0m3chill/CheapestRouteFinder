45627659a8f6717cd749356ee38cd6a2da08c5da
dariy.kordiyak <dariy.kordiyak@avenga.com>
2023-12-12 11:18:18 +0200

    Add updated tests for alternative route

--- CheapestRouteFinder/CheapestRouteFinderTests/CheapestRouteCalculatorTests.swift
+++ CheapestRouteFinder/CheapestRouteFinderTests/CheapestRouteCalculatorTests.swift
@@ -9,38 +9,47 @@ import XCTest
 @testable import CheapestRouteFinder
 
 final class CheapestRouteCalculatorTests: XCTestCase {
-    
-    // MARK: - Properties
-    private var sut: CheapestRouteCalculator!
-    
-    // MARK: - Setup
-    override func setUp() {
-        super.setUp()
         
-        let connections = ConnectionsDataStub().cities
-        sut = CheapestRouteCalculator(connections: connections)
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-        
-        sut = nil
-    }
-    
     // MARK: - Tests
     func testCalculateCheapestRouteIfRouteExists() {
+        let connections = ConnectionsDataStub().cities
+        let sut = CheapestRouteCalculator(connections: connections)
         let (route, price) = sut.calculateCheapestRoute(from: "Tokyo", to: "London")
         XCTAssertEqual(route.map { $0.from }, ["Tokyo"])
         XCTAssertEqual(price, 200)
     }
     
+    func testCalculateSuperCheapRoute() {
+        var connections = ConnectionsDataStub().cities
+        connections.append(Connection(from: "Tokyo", to: "London",
+                                      coordinates: .init(from: .init(lat: 1, long: 1),
+                                                         to: .init(lat: 2, long: 2)),
+                                      price: 5))
+        let sut = CheapestRouteCalculator(connections: connections)
+        let (route, price) = sut.calculateCheapestRoute(from: "Tokyo", to: "London")
+        XCTAssertEqual(route.map { $0.from }, ["Tokyo"])
+        XCTAssertEqual(price, 5)
+    }
+    
+    func testCalculateAlternativeRouteUpdatedPrice() {
+        let connections = ConnectionsDataStub().citiesUpdatePriceFromTokyo
+        let sut = CheapestRouteCalculator(connections: connections)
+        let (route, price) = sut.calculateCheapestRoute(from: "Tokyo", to: "London")
+        XCTAssertEqual(route.map { $0.from }, ["Tokyo", "Sydney", "Cape Town"])
+        XCTAssertEqual(price, 3)
+    }
+    
     func testCalculateCheapestRouteIfRouteDoesNotExist() {
+        let connections = ConnectionsDataStub().cities
+        let sut = CheapestRouteCalculator(connections: connections)
         let (route, price) = sut.calculateCheapestRoute(from: "Porto", to: "London")
         XCTAssertEqual(route.count, 0)
         XCTAssertEqual(price, 0)
     }
     
     func testCalculateCheapestRouteToSameLocation() {
+        let connections = ConnectionsDataStub().cities
+        let sut = CheapestRouteCalculator(connections: connections)
         let (route, price) = sut.calculateCheapestRoute(from: "London", to: "London")
         XCTAssertEqual(route.count, 0)
         XCTAssertEqual(price, 0)
--- CheapestRouteFinder/CheapestRouteFinderTests/Mocks/ConnectionsDataStub.swift
+++ CheapestRouteFinder/CheapestRouteFinderTests/Mocks/ConnectionsDataStub.swift
@@ -20,4 +20,16 @@ struct ConnectionsDataStub {
         Connection(from: "New York", to: "Los Angeles", coordinates: .init(from: .init(lat: 0, long: 0), to: .init(lat: 3, long: 3)), price: 120),
         Connection(from: "Los Angeles", to: "Tokyo", coordinates: .init(from: .init(lat: 0, long: 0), to: .init(lat: 3, long: 3)), price: 150),
     ]
+    
+    let citiesUpdatePriceFromTokyo = [
+        Connection(from: "London", to: "Tokyo", coordinates: .init(from: .init(lat: 0, long: 0), to: .init(lat: 1, long: 1)), price: 220),
+        Connection(from: "Tokyo", to: "London", coordinates: .init(from: .init(lat: 1, long: 1), to: .init(lat: 2, long: 2)), price: 200),
+        Connection(from: "London", to: "Porto", coordinates: .init(from: .init(lat: 2, long: 2), to: .init(lat: 3, long: 3)), price: 50),
+        Connection(from: "Tokyo", to: "Sydney", coordinates: .init(from: .init(lat: 0, long: 0), to: .init(lat: 3, long: 3)), price: 1),
+        Connection(from: "Sydney", to: "Cape Town", coordinates: .init(from: .init(lat: 0, long: 0), to: .init(lat: 1, long: 1)), price: 1),
+        Connection(from: "Cape Town", to: "London", coordinates: .init(from: .init(lat: 1, long: 1), to: .init(lat: 2, long: 2)), price: 1),
+        Connection(from: "London", to: "New York", coordinates: .init(from: .init(lat: 2, long: 2), to: .init(lat: 3, long: 3)), price: 400),
+        Connection(from: "New York", to: "Los Angeles", coordinates: .init(from: .init(lat: 0, long: 0), to: .init(lat: 3, long: 3)), price: 120),
+        Connection(from: "Los Angeles", to: "Tokyo", coordinates: .init(from: .init(lat: 0, long: 0), to: .init(lat: 3, long: 3)), price: 150),
+    ]
 }
